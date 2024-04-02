a563a18b6ae0b807adb1a11f4b65411720325904
Quentin Honreck <horneck.quentin@gmail.com>
2024-04-02 15:02:04 +0200

    maj

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Menu from "../../containers/Menu";
 import EventCard from "../../components/EventCard";
+import PeopleCard from "../../components/PeopleCard";
 import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
@@ -40,10 +41,6 @@ const Home = () => {
           <h2 className="Title">Nos services</h2>
           <p>Nous organisons des événements sur mesure partout dans le monde</p>
           <div className="ListContainer">
-            {/* Contenu des services */}
-            {/* ServiceCard 1 */}
-            {/* ServiceCard 2 */}
-            {/* ServiceCard 3 */}
           </div>
         </section>
         <section className="EventsContainer">
@@ -54,8 +51,37 @@ const Home = () => {
           <h2 className="Title">Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’organisation de vos événements</p>
           <div className="ListContainer">
-            {/* Contenu de l'équipe */}
-          </div>
+          <PeopleCard
+            imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
+            name="Samira"
+            position="CEO"
+          />
+          <PeopleCard
+            imageSrc="/images/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.png"
+            name="Jean-baptiste"
+            position="Directeur marketing"
+          />
+          <PeopleCard
+            imageSrc="/images/christina-wocintechchat-com-SJvDxw0azqw-unsplash.png"
+            name="Alice"
+            position="CXO"
+          />
+          <PeopleCard
+            imageSrc="/images/jonas-kakaroto-KIPqvvTOC1s-unsplash.png"
+            name="Luís"
+            position="Animateur"
+          />
+          <PeopleCard
+            imageSrc="/images/amy-hirschi-b3AYk8HKCl0-unsplash1.png"
+            name="Christine"
+            position="VP animation"
+          />
+          <PeopleCard
+            imageSrc="/images/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.png"
+            name="Isabelle"
+            position="VP communication"
+          />
+        </div>
         </section>
         <div className="FormContainer" id="contact">
           <h2 className="Title">Contact</h2>
